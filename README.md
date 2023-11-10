6cd68a7e19f787f4f08baab979c6c6f14f8e4845
Volodymyr <buvoro@gmail.com>
2023-11-10 17:22:24 +0200

    done2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # node-hw-01
+
+https://i.imgur.com/yDu3TRN.png
--- contacts.js
+++ contacts.js
@@ -43,7 +43,7 @@ async function addContact(name, email, phone) {
   contacts.push(newContact);
 
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 4));
-  return newContact;
+  return JSON.stringify(newContact);
   // Повертає об'єкт доданого контакту.
 }
 
